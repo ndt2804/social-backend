ce2b569ef0888479338a0149cd1c386ca3e66ce9
ndt2804 <nduytan.uit.a3@gmail.com>
2024-06-24 12:30:27 +0700

    feat: done api login user, getUser

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import {
   registerUserService,
   loginUserService,
+  userService,
+  getUserService,
 } from "../services/auth.service.js";
 
 export async function registerUser(req, res) {
@@ -18,10 +20,22 @@ export async function logInUser(req, res) {
   const { email, password } = req.body;
   try {
     const user = await loginUserService(email, password);
+    res.cookie('accessToken', user.accessToken, { httpOnly: true, secure: true });
+    res.cookie('refreshToken', user.refreshToken, { httpOnly: true, secure: true });
     res.status(201).json(user);
+
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Login failed" });
+  }
+}
+
+export async function getProfile(req, res) {
+  try {
+    const user = await userService(req.headers.cookie);
+    res.status(200).send(user);
+  } catch (error) {
+    return res.status(500).send((error));
   }
 }
 
@@ -39,5 +53,17 @@ export async function refreshToken(req, res) {
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Login failed" });
+  }
+}
+
+export async function updateUser(req, res) {
+
+}
+export async function getUser(req, res) {
+  try {
+    const user = await getUserService(req.params.slug);
+    res.status(200).send(user);
+  } catch (error) {
+    return res.status(500).send((error));
   }
 }
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,24 +1,29 @@
-import jwt, { Secret, JwtPayload } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import "dotenv/config";
 const secretKey = process.env.SECRET_KEY;
 
 export const auth = async (req, res, next) => {
   try {
-    const token = req.cookies.accessToken;
-
-    if (!token) {
+    const cookie = req.headers.cookie;
+    if (cookie) {
+      const accessTokenMatch = cookie.match(/accessToken=([^;]*)/);
+      const refreshTokenMatch = cookie.match(/refreshToken=([^;]*)/);
+      const accessToken = accessTokenMatch ? accessTokenMatch[1] : null;
+      const refreshToken = refreshTokenMatch ? refreshTokenMatch[1] : null
+      jwt.verify(accessToken, secretKey, (err, user) => {
+        if (err) {
+          return res
+            .status(401)
+            .json({ message: "Failed to authenticate token." });
+        }
+        req.user = user;
+        next();
+      });
+    }
+    if (!cookie) {
       return res.status(403).json({ message: "You do not have authorization" });
     }
 
-    jwt.verify(token, secretKey, (err, user) => {
-      if (err) {
-        return res
-          .status(401)
-          .json({ message: "Failed to authenticate token." });
-      }
-      req.user = user;
-      next();
-    });
   } catch (err) {
     console.log(err);
     res.status(401).send("Please authenticate");
