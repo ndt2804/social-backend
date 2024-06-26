a6bcc1dbda2772b1cc7bb39bc1a152c471b837e7
ndt2804 <nduytan.uit.a3@gmail.com>
2024-06-26 17:00:42 +0700

    feat: done resetPassword

--- api/auth.js
+++ api/auth.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser, logInUser, getProfile, getUser } from "../controllers/auth.controller.js";
+import { registerUser, logInUser, getProfile, getUser, updateUser, changePassword, resetPassword, updatePassword, verifyEmail } from "../controllers/auth.controller.js";
 import { auth } from '../middlewares/auth.js'
 
 const authRouter = Router();
@@ -8,5 +8,10 @@ authRouter.post("/auth/register", registerUser);
 authRouter.post("/auth/login", logInUser);
 authRouter.get('/user/me', auth, getProfile)
 authRouter.get('/user/:slug', getUser)
+authRouter.put('/user/:slug/settings', auth, updateUser)
+authRouter.put('/user/:slug/changePassword', auth, changePassword)
+authRouter.post('/resetPassword', resetPassword)
+authRouter.get('/updatePassword', verifyEmail)
+authRouter.post('/updatePassword', updatePassword)
 
 export default authRouter;
